edef828d65ad81946233c0c4daac4f1eda314262
aadityamahindru <aaditya2069@gmail.com>
2020-09-17 09:56:33 +0530

    amazon clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQviXccVY1d2DlWXnsJ0TKsVUAUD7nDgw",
+    authDomain: "clone-3497a.firebaseapp.com",
+    databaseURL: "https://clone-3497a.firebaseio.com",
+    projectId: "clone-3497a",
+    storageBucket: "clone-3497a.appspot.com",
+    messagingSenderId: "34551890911",
+    appId: "1:34551890911:web:d79a27c15256ecc08a3a6a",
+    measurementId: "G-GXWN8CD45X"
+  };
