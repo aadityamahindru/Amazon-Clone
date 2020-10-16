9ef6a6ddb9658f2b0ad131c5ccb91a5809808cc0
aadityamahindru <aaditya2069@gmail.com>
2020-10-17 05:03:45 +0530

    auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 const firebaseConfig = {
     apiKey: "AIzaSyCQviXccVY1d2DlWXnsJ0TKsVUAUD7nDgw",
     authDomain: "clone-3497a.firebaseapp.com",
@@ -8,3 +9,9 @@ const firebaseConfig = {
     appId: "1:34551890911:web:d79a27c15256ecc08a3a6a",
     measurementId: "G-GXWN8CD45X"
   };
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig); 
+  const db=firebaseApp.firestore();
+  const auth=firebase.auth();
+
+  export {db,auth};
--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,10 @@
 export const initialState = {
     basket: [],
+    user:null,
 }
 export const getBasketTotal=(basket)=>
     basket?.reduce((amount,item)=> item.price+amount,0);
 const reducer = (state, action) => {
-    console.log(action)
     switch (action.type) {
         case 'ADD_TO_BASKET':
             return {
@@ -24,6 +24,11 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 basket: newBasket
+            }
+        case "SET_USER":
+            return{
+                ...state,
+                user:action.user
             }
         default:
             return state;
